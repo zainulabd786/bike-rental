1967bdd56abd562268df8d889ef7f6b6a7c5f54d
Zainul Abideen <waeezazeem@gmail.com>
2022-08-24 17:03:46 +0530

    Creates mock API server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import jsonServer from 'json-server';
+import auth from "json-server-auth"
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.db = router.db
+
+server.use(middlewares)
+server.use(auth)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
