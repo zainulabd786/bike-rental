1175c406bc9c8334d5956ce0a7f90bb29470c0d0
Zainul Abideen <waeezazeem@gmail.com>
2022-08-24 17:10:00 +0530

    Updates API server port to 3001

--- server/index.js
+++ server/index.js
@@ -10,6 +10,6 @@ server.db = router.db
 server.use(middlewares)
 server.use(auth)
 server.use(router)
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log('JSON Server is running')
 })
