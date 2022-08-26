094c8c8b7260b64f177902565dc1fa955f51f26e
Zainul Abideen <zabideen@ebay.com>
2022-08-26 18:15:28 +0530

    Fix BikeCard styles

--- client/src/components/Home/Card.js
+++ client/src/components/Home/Card.js
@@ -1,32 +1,40 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import {Card, Button} from '@mui/material/';
+import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { green } from '@mui/material/colors';
 
-export default function BikeCard({model, color, location, id, available}) {
+
+export default function BikeCard({model, color, location, id, available, rating}) {
   return (
     <Card sx={{ width: '100%' }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/bike.webp"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {model}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {color}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" disabled={!available}>
-          Book
-        </Button>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: available ? green[500] : '' }} aria-label="recipe">
+            {id}
+          </Avatar>
+        }
+        title={model}
+        subheader={color}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="/bike.webp"
+        alt={model}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {location}
+        </Typography>
+      </CardContent>
+      <CardActions className='justify-content-between' disableSpacing>
+        <Typography>Rating: {rating}</Typography>
+        <Button disabled={!available}>Book</Button> 
       </CardActions>
     </Card>
   );
